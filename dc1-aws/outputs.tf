b55f871eff407c9a7884393a051de4fb7ad19a49
Krastin Krastev <krastin@hashicorp.com>
2024-01-23 14:20:39 +0100

    clean up dc1 output

--- dc1-aws/outputs.tf
+++ dc1-aws/outputs.tf
@@ -15,24 +15,7 @@ output "region" {
   value = var.vpc_region
 }
 
-output "vpc" {
-  value = {
-    vpc_id         = module.vpc.vpc_id
-    vpc_cidr_block = module.vpc.vpc_cidr_block
-    hvn_cidr_block = var.hvn_cidr_block
-  }
-}
-
 output "hcp_consul_ca" {
   value = base64decode(hcp_consul_cluster.main.consul_ca_file)
   sensitive = true
-}
-
-output "next_steps" {
-  value = <<-EOT
-    aws eks --region $(terraform -chdir=dc1-aws output -raw region) update-kubeconfig --name $(terraform -chdir=dc1-aws output -raw cluster_name) --alias=dc1
-    kubectl --context=dc1 create namespace consul
-    kubectl --context=dc1 --namespace=consul create secret generic ${hcp_consul_cluster.main.datacenter} --from-literal="caCert=$(terraform -chdir=dc1-aws output -raw hcp_consul_ca)" --from-literal="bootstrapToken=$(terraform -chdir=dc1-aws output -raw consul_token)"
-    consul-k8s install -context=dc1 -config-file=k8s-yamls/consul-helm-dc1.yaml
-  EOT
 }
